a9bc6160337f13cd2561b4b513b0858f64582831
lisgroup <lisgroup@qq.com>
2024-04-08 09:13:35 +0800

    test: PUT and GET method

--- db_open_test.go
+++ db_open_test.go
@@ -18,3 +18,23 @@ func TestPut(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestKvDB_Get(t *testing.T) {
+	kv, err := Open("kv.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 测试Get方法
+	val, err := kv.Get([]byte("key1"))
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(string(val))
+
+	val, err = kv.Get([]byte("key2"))
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(string(val))
+}
